a18ca519eb6b68bfe9535c9e062c8d2ee51a3563
PixelBoyz <stevekaurn@gmail.com>
2022-08-01 14:03:21 +0530

    deleted:    backend/.env.example 	deleted:    backend/layers/Background/Black.png 	modified:   backend/layers/Background/Blue.png 	new file:   backend/layers/Background/Green.png 	new file:   backend/layers/Background/Grey.png 	modified:   backend/layers/Background/Orange.png 	new file:   backend/layers/Background/Red.png 	modified:   backend/layers/Background/Yellow.png 	deleted:    backend/layers/Bottom lid/High High #20.png 	deleted:    backend/layers/Bottom lid/Low#40.png 	deleted:    backend/layers/Bottom lid/Middle#40.png 	deleted:    backend/layers/Eye color/Cyan#1.png 	deleted:    backend/layers/Eye color/Green#1.png 	deleted:    backend/layers/Eye color/Pink#1.png 	deleted:    backend/layers/Eye color/Purple#1.png 	deleted:    backend/layers/Eye color/Red#1.png 	deleted:    backend/layers/Eye color/Yellow#10.png 	deleted:    backend/layers/Eyeball/Red#50.png 	deleted:    backend/layers/Eyeball/White#50.png 	deleted:    backend/layers/Goo/Green#1.png 	deleted:    backend/layers/Iris/Large#20.png 	deleted:    backend/layers/Iris/Medium#20.png 	deleted:    backend/layers/Iris/Small#60.png 	deleted:    backend/layers/Shine/Shapes#100.png 	new file:   backend/layers/Sword/Black.png 	new file:   backend/layers/Sword/Blue.png 	new file:   backend/layers/Sword/Purple.png 	deleted:    backend/layers/Top lid/High#30.png 	deleted:    backend/layers/Top lid/Low#20.png 	deleted:    backend/layers/Top lid/Middle#50.png 	modified:   backend/src/config.js 	modified:   frontend/js/constants.js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
+const contractAddress = "0x3AF4cD1bD89D3F30f281276605A691F4F05366b8"; // Replace with your own contract address
+const chain = 'polygon'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the sword NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The sword NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://Ankit.com"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
